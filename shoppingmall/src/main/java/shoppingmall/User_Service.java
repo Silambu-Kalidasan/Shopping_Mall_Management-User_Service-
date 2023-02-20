ef4c74c1b8ef5f8fc5fb2efd0f1712036ba2097e
Silambu-Kalidasan <121001963+Silambu-Kalidasan@users.noreply.github.com>
2023-02-20 12:06:35 +0530

    Add files via upload

---
+++ shoppingmall/src/main/java/shoppingmall/User_Service.java
@@ -0,0 +1,33 @@
+package shoppingmall;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
+@Transactional
+
+public class User_Service {
+
+	@Autowired
+	private User_Service_Repository repo;
+		public List<User> listAll()
+		{
+			return repo.findAll();
+		}
+		public void save(User u)
+		{
+			repo.save(u);
+		}
+		public User get(Integer u_id) 
+		{
+			return repo.findById(u_id).get();
+		}
+		public void delete(Integer u_id)
+		{
+			repo.deleteById(u_id);
+		}
+		
+}
